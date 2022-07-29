4fe9802c49ff8649946eec3c026a959d37e88bae
Luis Angel <luisparr14@gmail.com>
2022-07-29 18:38:54 -0500

    port env

--- config/database.js
+++ config/database.js
@@ -3,10 +3,11 @@ import { Sequelize } from 'sequelize';
 const DATABASE = process.env.DB_NAME;
 const USERNAME = process.env.DB_USER;
 const PASSWORD = process.env.DB_PASS;
+const PORT = process.env.DB_PORT;
 const HOST = process.env.DB_HOST;
-
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
+  port: PORT,
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   logging: false,
